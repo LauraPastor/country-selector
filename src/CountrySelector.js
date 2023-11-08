02cb69d7c30a506094caf3be9dd3ec5e5fda5bcc
LauraPastor <laurapastorj@gmail.com>
2023-11-08 16:34:31 +0100

    Fix scrolling with arrows keys

--- src/CountrySelector.js
+++ src/CountrySelector.js
@@ -11,26 +11,6 @@ const CountrySelector = () => {
   const inputRef = useRef(null);
 
   useEffect(() => {
-    const handleDocumentClick = (event) => {
-      const inputContainer = document.querySelector(".country-selector");
-      const searchResults = document.querySelector(".search-results");
-
-      if (
-        !inputContainer.contains(event.target) &&
-        !searchResults.contains(event.target)
-      ) {
-        searchResults.style.display = "none";
-      }
-    };
-
-    document.addEventListener("click", handleDocumentClick);
-
-    return () => {
-      document.removeEventListener("click", handleDocumentClick);
-    };
-  }, []);
-
-  useEffect(() => {
     const fetchCountries = async () => {
       try {
         const response = await axios.get("https://restcountries.com/v3.1/all");
@@ -41,11 +21,14 @@ const CountrySelector = () => {
     };
 
     fetchCountries();
+  }, []);
 
+  useEffect(() => {
     const filterCountries = () => {
       const filteredResults = countries
         .map((country) => {
-          const matchIndex = country.name.common
+          const countryName = String(country.name.common);
+          const matchIndex = countryName
             .toLowerCase()
             .indexOf(inputValue.toLowerCase());
           return {
@@ -53,16 +36,16 @@ const CountrySelector = () => {
             rank: matchIndex === -1 ? Infinity : matchIndex,
             highlightedName:
               matchIndex !== -1
-                ? `${country.name.common.substring(
+                ? `${countryName.substring(
                     0,
                     matchIndex
-                  )}<b>${country.name.common.substring(
+                  )}<b>${countryName.substring(
                     matchIndex,
                     matchIndex + inputValue.length
-                  )}</b>${country.name.common.substring(
+                  )}</b>${countryName.substring(
                     matchIndex + inputValue.length
                   )}`
-                : country.name.common,
+                : countryName,
           };
         })
         .filter((result) => result.rank !== Infinity)
@@ -80,7 +63,28 @@ const CountrySelector = () => {
   };
 
   const handleInputClick = () => {
+    // Show the search results when the input is clicked
     document.querySelector(".search-results").style.display = "block";
+
+    // Add a click event listener to the document
+    document.addEventListener("click", handleDocumentClick);
+  };
+
+  const handleDocumentClick = (event) => {
+    // Check if the click occurred outside the input and search results
+    const inputContainer = document.querySelector(".country-selector");
+    const searchResultsContainer = document.querySelector(".search-results");
+
+    if (
+      !inputContainer.contains(event.target) &&
+      !searchResultsContainer.contains(event.target)
+    ) {
+      // Hide the search results when clicking outside the input and search results
+      searchResultsContainer.style.display = "none";
+
+      // Remove the click event listener from the document
+      document.removeEventListener("click", handleDocumentClick);
+    }
   };
 
   const handleKeyDown = (event) => {
@@ -92,19 +96,40 @@ const CountrySelector = () => {
       setHoveredIndex((prevIndex) =>
         prevIndex === null ? null : Math.max(prevIndex - 1, 0)
       );
+      scrollResultsIntoView(hoveredIndex - 1);
     } else if (event.key === "ArrowDown") {
       setHoveredIndex((prevIndex) =>
         prevIndex === null
           ? 0
           : Math.min((prevIndex || 0) + 1, searchResults.length - 1)
       );
+      scrollResultsIntoView(hoveredIndex + 1);
+    }
+  };
+
+  const scrollResultsIntoView = (index) => {
+    const resultItem = document.querySelector(
+      `.search-results .result-item:nth-child(${index + 1})`
+    );
+
+    if (resultItem) {
+      // Scroll the selected result into view
+      resultItem.scrollIntoView({
+        behavior: "smooth",
+        block: "nearest",
+      });
     }
   };
 
   const handleSelection = (country) => {
     setSelectedCountry(country);
     setInputValue(country.name.common);
+
+    // Hide the search results after selecting a country
     document.querySelector(".search-results").style.display = "none";
+
+    // Remove the click event listener from the document
+    document.removeEventListener("click", handleDocumentClick);
   };
 
   const handleHover = (index) => {
@@ -122,16 +147,15 @@ const CountrySelector = () => {
         onClick={handleInputClick}
         placeholder="Type to search for a country"
       />
-      <div className="search-results">
+      <div className="search-results" style={{ overflowY: "scroll" }}>
         {searchResults.map((result, index) => (
           <div
             key={index}
             className={`result-item ${index === hoveredIndex ? "hovered" : ""}`}
             onClick={() => handleSelection(result.country)}
             onMouseEnter={() => handleHover(index)}
-          >
-            <div dangerouslySetInnerHTML={{ __html: result.highlightedName }} />
-          </div>
+            dangerouslySetInnerHTML={{ __html: result.highlightedName }}
+          />
         ))}
       </div>
       {selectedCountry && (
